befc386a9076b96e1bffc96f62de8a6317729b8f
penglunjian <875831103@qq.com>
2020-06-18 23:33:12 +0800

    1、项目优化

--- src/packageOther/pages/address/controller.js
+++ src/packageOther/pages/address/controller.js
@@ -4,10 +4,13 @@ export const STATES = {
   isLoading: state => state.SELECT_CITYS_REDUCER.isLoading,
   isSuccess: state => state.SELECT_CITYS_REDUCER.isSuccess,
   isFailure: state => state.SELECT_CITYS_REDUCER.isFailure,
-  isData: state => state.SELECT_CITYS_REDUCER.data
+  isData: state => state.SELECT_CITYS_REDUCER.data,
+  isCity: state => state.UPDATE_CITYS_REDUCER.data
 };
 
 export const ACTIONS = {
   ajaxSelectCitys: actions.ajaxRequestSelectCitys,
-  updateCitysReplace: params => actions.updateCitysReplace(params)
+  updateCitysReplace: params => actions.updateCitysReplace(params),
+  ajaxSelectAreas: params => actions.ajaxRequestSelectAreas(params),
+  ajaxSelectHotHouse: params => actions.ajaxRequestSelectHotHouse(params)
 };
